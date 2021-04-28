5f0f5455b5d0ac31580ab5ec89a0531791c6f3fb
Aaron Cleveland <toscleveland@gmail.com>
2021-04-28 14:27:02 -0500

    Added CustomViews UIButton & UITextfields

---
+++ GithubFollowersProject/GFButton.swift
@@ -0,0 +1,37 @@
+//
+//  GFButton.swift
+//  GithubFollowersProject
+//
+//  Created by Aaron Cleveland on 4/28/21.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    
+    // Subclass of UIButton we would need to overwrite the init if we want to customize it ** Programmatic UI **
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        configure()
+    }
+    
+    // Required Init is needed with the above override init. ** Storyboard UI **
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    // Configure will only be available to this class GFButton
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = .preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
