4a100578186644635c663977a379cc913c25b9ae
Aaron Cleveland <toscleveland@gmail.com>
2021-04-28 17:15:36 -0500

    Added GFBody and GFTitle

---
+++ GithubFollowersProject/GFBodyLabel.swift
@@ -0,0 +1,35 @@
+//
+//  GFBodyLabel.swift
+//  GithubFollowersProject
+//
+//  Created by Aaron Cleveland on 4/28/21.
+//
+
+import UIKit
+
+class GFBodyLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        font = .preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        lineBreakMode = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
---
+++ GithubFollowersProject/GFTitleLabel.swift
@@ -0,0 +1,36 @@
+//
+//  GFTitleLabel.swift
+//  GithubFollowersProject
+//
+//  Created by Aaron Cleveland on 4/28/21.
+//
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+        configure()
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+    }
+    
+}
