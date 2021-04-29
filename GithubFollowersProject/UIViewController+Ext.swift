251a4d19864421bac6f0731d5f9579d68cabbe57
Aaron Cleveland <toscleveland@gmail.com>
2021-04-29 00:05:45 -0500

    Created reusable custom alert

---
+++ GithubFollowersProject/UIViewController+Ext.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+Ext.swift
+//  GithubFollowersProject
+//
+//  Created by Aaron Cleveland on 4/28/21.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertViewController(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+}
