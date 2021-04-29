fc2a606497ac996d3f2309132d4e73b82260b101
Aaron Cleveland <toscleveland@gmail.com>
2021-04-29 00:43:39 -0500

    Created User and Follower model

---
+++ GithubFollowersProject/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GithubFollowersProject
+//
+//  Created by Aaron Cleveland on 4/29/21.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GithubFollowersProject/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GithubFollowersProject
+//
+//  Created by Aaron Cleveland on 4/29/21.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
