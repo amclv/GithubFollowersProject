ba78c77ec03fc2d6f0986ec647487e6671c093f3
Aaron Cleveland <toscleveland@gmail.com>
2021-04-30 21:23:34 -0500

    Create a collectionviewflowlayout

--- GithubFollowersProject/Screens/FollowerListViewController.swift
+++ GithubFollowersProject/Screens/FollowerListViewController.swift
@@ -41,9 +41,17 @@ class FollowerListViewController: UIViewController {
     }
     
     func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let mininumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (mininumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
         
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
         
-        return UICollectionViewFlowLayout()
+        return flowLayout
     }
     
     func getFollowers() {
