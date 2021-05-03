f5dfddb357e65f3f80d33a4d56c5af5c883fb25e
Aaron Cleveland <toscleveland@gmail.com>
2021-05-03 18:34:27 -0500

    Implemented Child view controllers

---
+++ GithubFollowersProject/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GithubFollowersProject
+//
+//  Created by Aaron Cleveland on 5/3/21.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
