84f605dfb2cde2caca3e3b6047b404d0e0e481f5
Aaron Cleveland <toscleveland@gmail.com>
2021-05-06 00:31:52 -0500

    Persistence working for retrieving and adding users

--- GithubFollowersProject/Screens/FavoriteListViewController.swift
+++ GithubFollowersProject/Screens/FavoriteListViewController.swift
@@ -12,5 +12,14 @@ class FavoriteListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        
+        PersistenceManager.retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                print(favorites)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
